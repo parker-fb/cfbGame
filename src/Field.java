78cd777d4976dd1343bf4680979722860d096b92
parker.letak27 <parkerletak@gmail.com>
2025-02-14 09:48:22 -0600

    started on the football game

--- src/Field.java
+++ src/Field.java
@@ -1,2 +1,42 @@
-package PACKAGE_NAME;public class Field {
+import javax.swing.*;
+import java.awt.*;
+
+public class Field extends JPanel {
+
+
+
+    private Player player1;
+    private Player player2;
+    private Player player3;
+    private Player player4;
+
+
+
+    public Field(){
+
+
+
+
+    }
+
+
+    @Override
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        g.setColor(Color.BLACK);
+        g.fillOval(200,710,30,30);
+        player1.draw(g);
+        player2.draw(g);
+        player3.draw(g);
+        player4.draw(g);
+
+
+
+
+        repaint();
+    }
+
+
+
+
 }
--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class Player {
+import java.awt.*;
+
+public class Player {
+
+    private int x,y,size;
+    private Color color;
+
+
+    public Player(){
+
+        x = (int)(Math.random()+20);
+        y = (int)(Math.random()+20);
+        size = 30;
+        color = Color.BLUE;
+
+
+    }
+
+    public void draw(Graphics g){
+        g.setColor(color);
+        g.fillOval(x, y, size, size);
+    }
+
+
 }
